d2cb982f7b2f5544e291a8a8ee62a3db8213c036
Evan K <ekcopersonal@gmail.com>
2024-04-30 17:23:09 -0400

    chore: refactor CLEANUP_TIME, cleanup documentation

--- src/hypersync_lancedb_pipe/db_manager.py
+++ src/hypersync_lancedb_pipe/db_manager.py
@@ -9,17 +9,17 @@ from lancedb import DBConnection
 @dataclass
 class Manager:
     """
-    Manager class that contains main functionality to read and write data to a LanceDB Table. 
+    Manager class that contains main functionality to create and write data LanceDB tables. 
     """
+    # CLEANUP_TIME determines the the frequency that lancedb will clean up the database and compact the files.
+    # Increase the number for less frequent cleanups and longer batch update times.
+    CLEANUP_TIME = 86400 / 24 / 60
 
     def write_db(self, table: lancedb.table, data: pl.DataFrame, merge_on: str = "block_number"):
         """
         Write a dataframe to a LanceDB table. If a dataframe doesn't exist, then create it with the given data. The default
         merge_on value is "block_number" because that is one of the most universal columns for blockchain data.
         """
-        # CLEANUP_TIME determines the the frequency that lancedb will clean up the database and compact the files.
-        # Increase the number for less frequent cleanups and longer batch update times.
-        CLEANUP_TIME = 86400 / 24 / 60
         try:
             db: DBConnection = lancedb.connect(table)
             # Try to open and merge data into existing table.
@@ -27,7 +27,7 @@ class Manager:
             table.compact_files()
             table.to_lance().optimize.optimize_indices()
             table.cleanup_old_versions(
-                datetime.timedelta(seconds=CLEANUP_TIME))
+                datetime.timedelta(seconds=self.CLEANUP_TIME))
 
             # Perform a "upsert" operation
             table.merge_insert(merge_on)   \
@@ -35,7 +35,7 @@ class Manager:
                 .execute(data)
 
             table.cleanup_old_versions(
-                older_than=datetime.timedelta(seconds=CLEANUP_TIME*2), delete_unverified=True
+                older_than=datetime.timedelta(seconds=self.CLEANUP_TIME), delete_unverified=True
             )
         # if the table doesn't exist, create it with the data
         except FileNotFoundError:
